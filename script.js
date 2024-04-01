d9960c22d5650687e4a2c1bfc0c19d3664942ecb
l0yzi <sinnloseexistenz@gmail.co>
2024-04-02 00:56:09 +0300

    trans_btn

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const anchors = document.querySelectorAll('a[href*="#"]')
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+
+
+
+
+let TransBtn =  document.getElementById('translate_btn')
+
+const Translate = () => {
+  let VisibleElems = document.querySelectorAll('.visible');
+  let HideElems = document.querySelectorAll('.hide');
+  for (let visibleEl of VisibleElems){
+    visibleEl.classList.remove('visible')
+    visibleEl.classList.add('hide')
+  }
+
+  for (let hideEl of HideElems){
+    hideEl.classList.remove('hide')
+    hideEl.classList.add('visible')
+  }
 }
+
+TransBtn.addEventListener("click", Translate)
